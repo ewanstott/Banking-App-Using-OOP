00d3798400781ca8a92d84b7ad0cebef546793a6
Ewan Stott <ewan.stott1@gmail.com>
2023-08-16 10:42:20 +0100

    commit

---
+++ banking_app_using_oop.py
@@ -0,0 +1,16 @@
+# Parent Class: User
+# Holds details about a user - done
+# Has a function to show users details
+
+# Child class: Bank
+# Stores details about the account balance
+# Stores details about the amount
+# Allows for deposits, withdraw, view_balance
+
+class User():
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def
